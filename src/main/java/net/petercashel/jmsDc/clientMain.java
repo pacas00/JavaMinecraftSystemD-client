7d148e58fdef3508e8bb343daa03a78f0258d580
Peter Cashel <pacas00@gmail.com>
2015-04-07 22:08:38 +1000

    External SSL from config

--- src/main/java/net/petercashel/jmsDc/clientMain.java
+++ src/main/java/net/petercashel/jmsDc/clientMain.java
@@ -15,6 +15,7 @@
  *******************************************************************************/
 package net.petercashel.jmsDc;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import net.petercashel.commonlib.threading.threadManager;
@@ -22,13 +23,38 @@ import net.petercashel.jmsDc.command.commandClient;
 import net.petercashel.nettyCore.client.clientCore;
 import net.petercashel.nettyCore.common.exceptions.ConnectionShuttingDown;
 import net.petercashel.nettyCore.server.serverCore;
+import net.petercashel.nettyCore.ssl.SSLContextProvider;
+import static net.petercashel.jmsDc.Configuration.*;
+
 
 public class clientMain {
 
 	public static boolean run = true;
+	public static String host = "127.0.0.1";
+	public static int port = 14444;
+	private static Boolean CLIMode;
+	private static String CLIPath;
 	
 	public static void main(String[] args) {
 		//init client console and network;
+		configInit();
+		clientCore.UseSSL = getDefault(getJSONObject(cfg, "clientSettings"), "serverSSLEnable", true);
+		
+		
+		if (getDefault(getJSONObject(getJSONObject(cfg, "clientSettings"), "SSLSettings"), "SSL_UseExternal", true)) {
+			SSLContextProvider.useExternalSSL = true;
+			SSLContextProvider.pathToSSLCert = getDefault(getJSONObject(getJSONObject(cfg, "clientSettings"), "SSLSettings"), "SSL_ExternalPath", (new File(configDir, "SSLCERT.p12").toPath().toString()));
+			SSLContextProvider.SSLCertSecret = getDefault(getJSONObject(getJSONObject(cfg, "clientSettings"), "SSLSettings"), "SSL_ExternalSecret", "secret");
+		}
+				
+		host = getDefault(getJSONObject(cfg, "clientSettings"), "serverAddress", "127.0.0.1");
+		port = getDefault(getJSONObject(cfg, "clientSettings"), "serverPort", 14444);
+		CLIMode = getDefault(getJSONObject(cfg, "clientSettings"), "serverCLIMode", false);
+		CLIPath = getDefault(getJSONObject(cfg, "clientSettings"), "serverCLIPath", "");
+
+		
+		
+		
 		commandClient.init();
 		reconnect();
 		
@@ -61,11 +87,12 @@ public class clientMain {
 	}
 
 	public static void reconnect() {
+		if (!CLIMode) {
 		threadManager.getInstance().addRunnable(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					clientCore.initializeConnection("127.0.0.1",14444);
+					clientCore.initializeConnection(host,port);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -73,6 +100,9 @@ public class clientMain {
 				}
 			}
 		});
+		} else {
+			
+		}
 		
 	}
 
